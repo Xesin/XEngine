6c23fe8d98bc827878c6765b494f70e764475a2e
Francisco Ferrer <francisco.fer.fer@gmail.com>
2018-03-05 18:32:25 +0100

    Created a 2DObject to split functionality and created new Light objects

--- source/gameObjects/GameObject.ts
+++ source/gameObjects/GameObject.ts
@@ -10,30 +10,18 @@ namespace XEngine {
 		public anchor: Vector3;
 		public rotation: number;
 
-		public width: number;
-		public height: number;
-		public isometric: boolean;
 		public pickeable: boolean;
 		public downPos: Vector3;
 		public posWhenDown: Vector3;
-		public color: number;
+
 		public body: any;
 		public persist: boolean;
-
-		public onClick: Signal;
-		public onInputDown: Signal;
-		public onInputOver: Signal;
-		public onInputUp: Signal;
-		public onInputLeft: Signal;
-		public inputEnabled: boolean;
-		public isInputDown: boolean;
-		public isInputOver: boolean;
 
 		public render: boolean;
 		public fixedToCamera: boolean;
 		public materials: Array<Material>;
-		public mask: GameObject;
-		public sprite: string;
+
+
 		public modelMatrix: Array<number>;
 		protected _uv: Array<number>;
 		protected indexDataBuffer: XEngine.DataBuffer16;
@@ -42,10 +30,6 @@ namespace XEngine {
 
 		protected indexBuffer: IndexBuffer;
 		protected vertexBuffer: VertexBuffer;
-
-		private _prevWidth: number;
-		private _prevHeight: number;
-		private _prevPos: any;
 
 		constructor(game: Game, posX = 0, posY = 0, posZ = 0) {
 			this.game = game;
@@ -57,22 +41,10 @@ namespace XEngine {
 			this.anchor = new XEngine.Vector3(0, 0);
 			this.rotation = 0;
 			this.transform.position.setTo(posX, posY, posZ);
-			this.onClick = new XEngine.Signal();
-			this.onInputDown = new XEngine.Signal();
-			this.onInputUp = new XEngine.Signal();
-			this.onInputOver = new XEngine.Signal();
-			this.onInputLeft = new XEngine.Signal();
-			this.inputEnabled = false;
+
 			this.render = true;
 			this.fixedToCamera = false;
-			this.isometric = false;
-			this.isInputDown = false;
 
-			this.width = 0;
-			this.height = 0;
-			this._prevWidth = 0;
-			this._prevHeight = 0;
-			this._prevPos = {x: 0, y: 0 };
 			this.materials = new Array();
 
 			this.vertDataBuffer = new XEngine.DataBuffer32(24 * 4);
@@ -89,8 +61,6 @@ namespace XEngine {
 
 			// this._setVertices(this.width, this.height, this.color, this._uv);
 
-			this.mask = null;
-
 			this.modelMatrix = mat4.create();
 
 			mat4.identity(this.modelMatrix);
@@ -98,7 +68,6 @@ namespace XEngine {
 			this.pickeable = false;
 			this.downPos = new XEngine.Vector3(0, 0);
 			this.posWhenDown = new XEngine.Vector3(0, 0);
-			this.color = (0xffffff >> 16) + (0xffffff & 0xff00) + ((0xffffff & 0xff) << 16);
 		}
 
 		public destroy () {
@@ -122,12 +91,6 @@ namespace XEngine {
 			}
 		}
 
-		public setColor(value: number, a = 1.0) {
-			this.color = value;
-			this.alpha = a;
-			this._setVertices(this.width, this.height, this.color, this._uv);
-		}
-
 		public kill () {
 			this.alive = false;
 		}
@@ -146,14 +109,11 @@ namespace XEngine {
 				translation[1] += this.game.camera.transform.position.y;
 				translation[2] += this.game.camera.transform.position.z;
 			}
-			let anchorX = Math.round(-(this.width * this.anchor.x));
-			let anchorY = Math.round(-(this.height * this.anchor.y));
 			mat4.translate(childMatrix, childMatrix, translation);
 			mat4.rotateX(childMatrix, childMatrix, this.transform.rotation.x * XEngine.Mathf.TO_RADIANS);
 			mat4.rotateY(childMatrix, childMatrix, this.transform.rotation.y * XEngine.Mathf.TO_RADIANS);
 			mat4.rotateZ(childMatrix, childMatrix, this.transform.rotation.z * XEngine.Mathf.TO_RADIANS);
 			mat4.scale(childMatrix, childMatrix, this.transform.scale.toArray());
-			mat4.translate(childMatrix, childMatrix, [anchorX, anchorY, 0]);
 			return childMatrix;
 		}
 
@@ -183,92 +143,11 @@ namespace XEngine {
 			this.materials[0].baseUniforms.modelMatrix.value = this.modelMatrix;
 			this.materials[0].updateUniforms(context);
 
-			if (this._prevHeight !== this.height ||
-				this._prevWidth !== this.width ||
-				this._prevPos.x !== this.transform.position.x ||
-				this._prevPos.y !== this.transform.position.y) {
-				this._setVertices(this.width, this.height, this.color, this._uv);
-				this._prevHeight = this.height;
-				this._prevWidth = this.width;
-				this._prevPos.x = this.transform.position.x;
-				this._prevPos.y = this.transform.position.y;
-			}
+
 			this.vertexBuffer.bind();
 			this.indexBuffer.bind();
 
 			context.drawElements(context.TRIANGLES, 6, context.UNSIGNED_SHORT, 0);
-		}
-
-		public rendermask(gl: WebGLRenderingContext) {
-			// disable color (u can also disable here the depth buffers)
-			gl.colorMask(false, false, false, false);
-
-			// Replacing the values at the stencil buffer to 1 on every pixel we draw
-			gl.stencilFunc(gl.ALWAYS, 1, 1);
-			gl.stencilOp(gl.REPLACE, gl.REPLACE, gl.REPLACE);
-
-			gl.enable(gl.STENCIL_TEST);
-			if (this.sprite) {
-				let cache_image = this.game.cache.image(this.sprite);
-				(this.materials[0] as SpriteMat)._setTexture(cache_image._texture);
-			}
-
-			this.materials[0].baseUniforms.pMatrix.value = this.game.camera.pMatrix;
-			this.materials[0].updateUniforms(gl);
-
-			this.vertexBuffer.bind();
-			this.indexBuffer.bind();
-
-			gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-			// enabling back the color buffer
-			// Telling the stencil now to draw/keep only pixels that equals 1 - which we set earlier
-			gl.stencilFunc(gl.EQUAL, 1, 1);
-			gl.stencilOp(gl.ZERO, gl.ZERO, gl.ZERO);
-			gl.colorMask(true, true, true, true);
-		}
-
-		public endRendermask(gl: WebGLRenderingContext) {
-			gl.disable(gl.STENCIL_TEST);
-			gl.clear(gl.STENCIL_BUFFER_BIT);
-		}
-
-		public _endRender(gl: WebGLRenderingContext) {
-			if (this.mask != null) {
-				return;
-			}
-		}
-
-		public getBounds (): any {
-			let width = this.width * this.transform.scale.x;
-			let height = this.height * this.transform.scale.y;
-			let worldPos = this.getWorldPos();
-			let widthAnchor = width * this.anchor.x;
-			let heightAnchor = height * this.anchor.y;
-			let minX = worldPos.x - widthAnchor;
-			let maxX = worldPos.x + width - widthAnchor;
-			let minY = worldPos.y - heightAnchor;
-			let maxY = worldPos.y + height - heightAnchor;
-			return {
-				height: height,
-				maxX: maxX,
-				maxY: maxY,
-				minX: minX,
-				minY: minY,
-				width: width,
-			};
-		}
-
-		public isInsideCamera(): boolean {
-			let bounds = this.getBounds();
-			let worldPos = this.getWorldPos();
-			let cameraPos = this.game.camera.transform.position;
-			let viewRect = {width: this.game.width, height: this.game.height};
-			if (bounds.maxX < cameraPos.x) { return false; }
-			if (bounds.maxY < cameraPos.y) { return false; }
-			if (bounds.minX > cameraPos.x + viewRect.width) {return false; }
-			if (bounds.minY > cameraPos.y + viewRect.height) { return false; }
-
-			return true;
 		}
 
 		public start () { return; }
@@ -337,27 +216,6 @@ namespace XEngine {
 
 			this.vertexBuffer.updateResource(floatBuffer, 0);
 			this.indexBuffer.updateResource(uintIndexBuffer, 0);
-		}
-
-		public _setVertices(width: number, height: number, color: number, uv: Array<number>) {
-			let pos = new XEngine.Vector3(0, 0);
-			this._uv = uv;
-			this.width = width;
-			this.height = height;
-
-			let vertices = [
-				// Cara delantera
-				0, 0, -1,
-				width, 0, -1,
-				width, height, -1,
-				0, height, -1,
-			];
-			const indices = [
-				0, 1, 2,
-				1, 3, 2,
-			];
-
-			this.setVertices(vertices, indices, uv);
 		}
 
 		private _setBuffers() {
---
+++ source/gameObjects/lights/Light.ts
@@ -0,0 +1,7 @@
+namespace XEngine {
+	export class Light extends GameObject {
+		public type: LightType;
+		public intensity: number;
+		public lightColor: Vector3;
+	}
+}
