c1a986414625d6d48b6f0fbdd42542e1603c1ac3
Francisco Ferrer <francisco.ferrer@logitravelgroup.com>
2020-01-02 16:55:15 +0100

    Updated packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,6 @@
 var gulp        = require("gulp"),
-    tsc         = require("gulp-typescript"),
-    sourcemaps  = require("gulp-sourcemaps"),
     webpack     = require('webpack-stream');
 
-
-var tsProject = tsc.createProject("tsconfig.json");
-
-gulp.task("build-app", function() {
-    return tsProject.src()
-        .pipe(sourcemaps.init())
-        .pipe(tsProject())
-        .pipe(sourcemaps.write("../dist"))
-        .pipe(gulp.dest("dist"))
-});
 
 gulp.task("webpack", function() {
     return gulp.src("source")
@@ -35,12 +23,35 @@ gulp.task("webpack", function() {
                     { test: /\.tsx?$/, loader: "ts-loader" }
                 ]
             },
-            devtool: 'source-map'
+            mode: "production"
         }))
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task('build-pack', gulp.series(['build-app', 'webpack']), function()
-{
-    
+gulp.task("webpack-watch", function() {
+    return gulp.src("source")
+        .pipe(webpack({
+            entry: "./source/XEngine.ts",
+            optimization: {
+                minimize: true
+            },
+            output:
+            {
+                library: "XEngine",
+                filename: "XEngine_min.js"
+            },
+            resolve: {
+                extensions: [ '.tsx', '.ts', '.js' ],
+              },
+            module:
+            {
+                rules:[
+                    { test: /\.tsx?$/, loader: "ts-loader" }
+                ]
+            },
+            devtool: 'source-map',
+            mode: "development",
+            watch: true
+        }))
+        .pipe(gulp.dest("dist"));
 });
